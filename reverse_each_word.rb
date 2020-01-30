bf82efa12a40e5b209cd60402ae6f9509975b9c2
Jenise Ruehle <jela07@gmail.com>
2020-01-30 18:08:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   array1 = sentence.split
   array2 = []
-  array1.each { |words| array2 << sentence.reverse }
+  array1.each do |words| 
+    array2 << sentence.reverse }
 end   
   array2.join(" ")
 end  
