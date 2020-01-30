4e806c9eef697f87bc3004a79548be8d7753dbfe
Jenise Ruehle <jela07@gmail.com>
2020-01-30 17:44:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence)
-  array =[]
-  sentence = ["Hello there, and how are you?"]
+  array = []
   sentence.each do |words|
-    words.join
+    words.reverse 
   end 
-  
+  array.join(" ")
 end   
